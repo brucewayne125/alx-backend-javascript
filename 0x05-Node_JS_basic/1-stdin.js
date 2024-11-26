dde2d51102706fb6ed0640dcd57beb317e04d47e
brucewayne125 <snowdbat@gmail.com>
2024-11-26 02:48:21 -0800

    Task1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+
+console.log('Welcome to Holberton School, what is your name?');
+
+
+process.stdin.on('data', (data) => {
+	const input = data.toString().trim();
+	console.log(`Your name is: ${input}`);
+	process.exit();
+});
+
+process.on('exit', () => {
+	console.log('This important software is now closing');
+});
