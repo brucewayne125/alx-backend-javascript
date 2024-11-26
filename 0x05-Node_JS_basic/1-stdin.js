e805ee677d1543f05d1103f613bf288839364028
brucewayne125 <snowdbat@gmail.com>
2024-11-26 02:56:31 -0800

    Task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,8 +4,9 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 
+process.stdin.setEncoding('utf-8');
 process.stdin.on('data', (data) => {
-	const input = data.toString().trim();
+	const input = data.trim();
 	console.log(`Your name is: ${input}`);
 	process.exit();
 });
