cbc218cd7788ba583427ec63644477343edaaf9d
brucewayne125 <snowdbat@gmail.com>
2024-11-26 03:27:34 -0800

    Task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,12 +2,26 @@ console.log('Welcome to Holberton School, what is your name?');
 
 
 process.stdin.setEncoding('utf-8');
+
+
+let Piped = false;
+
+
+if (!process.stdin.isTTY){
+	Piped = true;
+}
 process.stdin.on('data', (data) => {
 	const input = data.trim();
 	console.log(`Your name is: ${input}`);
+	if(Piped){
+		console.log('This important software is now closing');
+	}
 	process.exit();
 });
 
-process.on('exit', () => {
-	console.log('This important software is now closing');
+process.on('SIGINT', () => {
+	if (Piped){
+		console.log('This important software is now closing');
+	}
+	process.exit();
 });
