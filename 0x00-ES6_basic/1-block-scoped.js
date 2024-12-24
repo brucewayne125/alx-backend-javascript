82a75c5799efe6c677fb3613bb4e56d71a849518
root <snowdbat@gmail.com>
2024-12-24 12:24:37 +0000

    1-block-scoped.js

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    // left empty for future conditionals
+  }
+
+  return [task, task2];
+}
