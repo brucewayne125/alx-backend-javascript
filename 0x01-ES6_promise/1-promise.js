925c2f283dcdce8d11a4a124dcbc0eefe512fcf5
root <snowdbat@gmail.com>
2024-12-28 13:41:41 +0000

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,16 +1,12 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if(success) {
+    if (success) {
       resolve({
         status: 200,
         body: 'Success',
       });
     } else {
-      const error = new Error('The fake API is not working currently');
-      console.error(error);
-      reject(error);
+      reject(new Error('The fake API is not working currently'));
     }
-  }).catch((error) => {
-    return Promise.reject(error);
   });
 }
