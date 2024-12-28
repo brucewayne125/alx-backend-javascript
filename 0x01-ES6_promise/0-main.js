1f112fb2f5c68efb556aadeaccd5c93a7e58b80f
root <snowdbat@gmail.com>
2024-12-28 12:44:11 +0000

    0-promise.js

--- 0x01-ES6_promise/0-main.js
+++ 0x01-ES6_promise/0-main.js
@@ -2,7 +2,3 @@ import getResponseFromAPI from "./0-promise.js";
 
 const response = getResponseFromAPI();
 console.log(response instanceof Promise);
-
-response
-  .then((data) => console.log(data))
-  .catch((error) => console.error(error));
--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,14 +1,5 @@
-const get getResponseFromAPI() = () => {
-	return new promise((resolve, reject) =>{
-		setTimeout(() => {
-			const success = true;
-			if(success){
-				resolve("response from API");
-			}else(
-				reject(new error("Error response from API"));
-			)
-		}, 1000);
-	});
-};
-
-export default getResponseFromAPI;
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    resolve();
+  });
+}
